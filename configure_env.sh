6dc0f2a1aeb7bf7c1c5bf0f10c09c3d01ac433d2
TTanujSoni84 <ttanujsoni84@gmail.com>
2021-03-08 13:45:23 +0530

    added env sample file

--- configure_env.sh
+++ configure_env.sh
@@ -6,18 +6,23 @@ PUBIP="$(cat PUBLIC_IP.TXT)"
 ## Working Directory of project.
 PROJECT_PATH="$(pwd)"
 
-# Jenkins admin password.
-ADMIN_PASS=admin123
+ADMIN_PASS="${ADMIN_PASS}"
 
-# Pem file path for the EC2 instance.
-PEM_FILE_PATH="<PEM File path here>"
+# Add Pem file path for the EC2 instance.
+PEM_FILE_PATH="${PEM_FILE_PATH}"
 
+# Add Github usernmae and password
+GITHUB_USERNAME="${GITHUB_USERNAME}"
+GITHUB_PASSWORD="${GITHUB_PASSWORD}"
 
 ## IP replaced in varibles file
 sed -i 's/PUBLIC_IP/'$PUBIP'/g' ansible/variables.yml
+
 ## IP replaced in hosts file
+
 sed -i 's/PUBLIC_IP/'$PUBIP'/g' ansible/hosts
 ## Pem file path replaced in hosts file
+
 sed -i 's|AWS_PRIVATE_KEY_PATH|'$PEM_FILE_PATH'|g' ansible/hosts
 
 sed -i 's|PROJECT_DIR|'$PROJECT_PATH'|g' ansible/variables.yml
@@ -25,5 +30,8 @@ sed -i 's|PROJECT_DIR|'$PROJECT_PATH'|g' ansible/variables.yml
 sed -i 's/ADMIN_PASSWORD/'$ADMIN_PASS'/g' ansible/variables.yml
 
 sed -i 's/ADMIN_PASSWORD/'$ADMIN_PASS'/g' ansible/setup-jenkins-cli.sh
+
+sed -i 's|GIT_USERNAME|'$GITHUB_USERNAME'|g' ansible/cred.xml
+sed -i 's|GIT_PASSWORD|'$GITHUB_PASSWORD'|g' ansible/cred.xml
 
 echo "Instance Public IP is '$PUBIP' and configuration is updated.."
